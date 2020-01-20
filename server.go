6efd910c54f7c94b67c80d588fbb59cbc2139a0a
cliu2 <cliu2@vmware.com>
2020-01-20 10:42:58 +0800

    modify flags for server-end

--- server.go
+++ server.go
@@ -8,14 +8,14 @@ import (
 )
 
 func main() {
-	client := flag.Int("LocalPort", utils.ClientPort, "client access port")
-	server := flag.Int("RemotePort", utils.ServerPort, "Service access port")
+	localPort := flag.Int("LocalPort", utils.ClientPort, "client access port")
+	remotePort := flag.Int("RemotePort", utils.ServerPort, "Service access port")
 	flag.Parse()
 	if flag.NFlag() != 2 {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 	ts := utils.NewServer()
-	ts.StartService(*client, *server)
+	ts.StartService(*localPort, *remotePort)
 	select {}
 }
